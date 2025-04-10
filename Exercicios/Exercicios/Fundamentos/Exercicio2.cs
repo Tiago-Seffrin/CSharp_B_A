7227b3da2f3a69b38cea41f5205de90b5a501e77
Tiago S Filippio <seffrin.ts@gmail.com>
2025-04-10 19:46:26 -0300

    novos exercícios

---
+++ Exercicios/Exercicios/Fundamentos/Exercicio2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios.Fundamentos
+{
+    class Exercicio2
+    {
+        public static void Executar()
+        {
+            double raio;
+            double area;
+            double TT = 3.14159;
+            
+            Console.WriteLine("Digite o valor do raio: ");
+            raio = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Fórmula usada será area = TT . raio2");
+
+            area = TT * Math.Pow(raio, 2);
+            Console.WriteLine($"Valor do raio: {raio}. Valor da área: {area:f4}");
+            Console.ReadLine();
+
+        }
+    }
+}
--- Exercicios/Exercicios/Program.cs
+++ Exercicios/Exercicios/Program.cs
@@ -8,6 +8,7 @@ namespace CursoCSharp {
         static void Main(string[] args) {
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
                 {"Primeiro Programa - Fundamentos", Exercicio1.Executar},
+                {"Segundo Programa - Fundamentos", Exercicio2.Executar},
             });
 
             central.SelecionarEExecutar();
